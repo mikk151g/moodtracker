6aef57e8f89afe87cd6cbda47835fa3b5d8b116d
Mikkel Hartig Braunstein <mikk151g@edu.campusvejle.dk>
2021-01-05 12:35:02 +0100

    :construction: Add ActivitiesList component

---
+++ src/components/ActivitiesList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="activity in getActivities" :key="activity.id">
+      <h2>{{ activity.title }}</h2>
+      <p>{{ activity.date }}</p>
+      <p>{{ activity.description }}</p>
+      <p>{{ activity.rating }}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["getActivities"]),
+  },
+};
+</script>
