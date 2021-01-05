3d59781cfd761a35b415a053816b5828f9ef4296
Mikkel Hartig Braunstein <mikk151g@edu.campusvejle.dk>
2021-01-05 13:08:25 +0100

    :lipstick: Add some styling

--- src/components/ActivitiesList.vue
+++ src/components/ActivitiesList.vue
@@ -1,10 +1,14 @@
 <template>
   <ul>
-    <li v-for="activity in getActivities" :key="activity.id">
-      <h2>{{ activity.title }}</h2>
-      <p>{{ activity.date }}</p>
+    <li
+      v-for="activity in getActivities"
+      :key="activity.id"
+      class="bg-white rounded p-4 m-4"
+    >
+      <h2 class="font-bold">{{ activity.title }}</h2>
+      <p class="text-gray-400">{{ activity.date }}</p>
       <p>{{ activity.description }}</p>
-      <p>{{ activity.rating }}</p>
+      <p>Rating: {{ activity.rating }}</p>
     </li>
   </ul>
 </template>
